ac35a7ec60e464206875e6c297b1d282f92f1f99
Ashik Kirmani <115408407+Ashikkirmani011@users.noreply.github.com>
2023-07-17 16:51:35 +0530

    Add files via upload

---
+++ ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by swamy kola on 17/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
